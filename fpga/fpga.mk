ea8293af3aba7988b8a5089db6fa8222609b8673
Edward Wang <edward.c.wang@compdigitec.com>
2018-07-27 12:48:52 -0400

    Update Verilog makefile

--- fpga/fpga.mk
+++ fpga/fpga.mk
@@ -15,11 +15,19 @@
 #-------------------------------------------------------------------------------
 # Generate Verilog Code
 #-------------------------------------------------------------------------------
-$(VERILOG): $(SRC_DIR)/$(MODULE)/*.scala
+$(VERILOG): $(SRC_DIR)/$(MODULE)/*.scala $(FIRRTL_JAR)
 	cd $(SBT_DIR) && \
-	$(SBT) "project Core" "run $(CORE_CONFIG) --compiler verilog --target-dir $(SBT_TO_BASE)/$(FPGA_SRC_DIR)"
-	sed -i -e '/^module ISpm/,/^endmodule/ s/\(reg \[31:0\] ispm \[\(.*\):0\];\)/\1  initial $$readmemh(\"ispmfile\", ispm, 0, \2);/g' $(VERILOG)
-	sed -i -e '/^module DSpm/,/^endmodule/ s/\(reg \[31:0\] dspm \[\(.*\):0\];\)/\1  initial $$readmemh(\"dspmfile\", dspm, 0, \2);/g' $(VERILOG)
+	$(SBT) "project Core" "run $(CORE_CONFIG) --compiler high --target-dir $(SBT_TO_BASE)/$(FPGA_SRC_DIR)"
+	# high-firrtl is dumped into $(FPGA_SRC_DIR)/$(MODULE).hi.fir
+
+	# Use FIRRTL to compile to Verilog
+	java -jar $(FIRRTL_JAR) --compiler verilog \
+		--input-file $(FPGA_SRC_DIR)/$(MODULE).hi.fir \
+		--target-dir $(FPGA_SRC_DIR) \
+		--top-name $(MODULE)
+
+	# TODO: do these in FIRRTL or use the new memory-loading feature of Chisel
+	sed -i -e '/^module ISpm/,/^endmodule/ s/\(reg \[31:0\] ispm \[[0-9]*:\([0-9]*\)\];\)/\1  initial $$readmemh(\"ispmfile\", ispm, 0, \2);/g' $(VERILOG)
+	sed -i -e '/^module DSpm/,/^endmodule/ s/\(reg \[31:0\] dspm \[[0-9]*:\([0-9]*\)\];\)/\1  initial $$readmemh(\"dspmfile\", dspm, 0, \2);/g' $(VERILOG)
+
 	cp $(VERILOG) fpga/generated-src/Core.v
-
-
