9c2fe4ea286e24767d7a76bec0412877161873b1
Edward Wang <edward.c.wang@compdigitec.com>
2018-07-27 21:57:22 -0400

    Refactor config parsing

--- src/Core/main.scala
+++ src/Core/main.scala
@@ -11,21 +11,11 @@ import chisel3._
 
 object CoreMain {
   def main(args: Array[String]): Unit = {
-
     val confString = args(0)
     val chiselArgs = args.slice(1, args.length)
-
-    val parsed = """(\d+)t(.*)-(\d+)i-(\d+)d.*-(.*)""".r.findFirstMatchIn(confString)
-    val coreConfig = new FlexpretConfiguration(
-      parsed.get.group(1).toInt,
-      !parsed.get.group(2).isEmpty,
-      parsed.get.group(3).toInt,
-      parsed.get.group(4).toInt,
-      confString contains "mul",
-      parsed.get.group(5)
-      )
+    val coreConfig = FlexpretConfiguration.parseString(confString)
 
     // Pass configuration to FlexPRET processor.
     chisel3.Driver.execute(chiselArgs, () => new Core(coreConfig))
-   }
+  }
 }
