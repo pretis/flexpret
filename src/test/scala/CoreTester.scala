2218ccd532fe714ae2146fc11b340b3bca05eed2
Edward Wang <edward.c.wang@compdigitec.com>
2018-07-27 23:30:31 -0400

    Check in initial tester sketch

---
+++ src/test/scala/CoreTester.scala
@@ -0,0 +1,32 @@
+/******************************************************************************
+File: CoreTester.scala
+Description: Testbench for FlexPRET processor.
+Author: Michael Zimmer (mzimmer@eecs.berkeley.edu)
+Contributors: Edward Wang (edwardw@eecs.berkeley.edu)
+License: See LICENSE.txt
+******************************************************************************/
+package Core.test
+
+import Core._
+import chisel3.iotesters._
+
+class CoreTester(c: Core) extends PeekPokeTester(c) {
+  finish
+}
+
+object CoreTesterMain {
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      System.err.println("Usage: configuration_string")
+      return
+    }
+    val confString = args(0)
+    val coreConfig = FlexpretConfiguration.parseString(confString)
+
+    // Use Verilator for C++ simulation.
+    val extraArgs = Array("--backend-name", "verilator")
+    Driver.execute(args ++ extraArgs, () => new Core(coreConfig)) {
+      c => new CoreTester(c)
+    }
+  }
+}
