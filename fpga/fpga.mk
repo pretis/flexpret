090006be20d12760ac65c1784c5e11faf0cfab67
Edward Wang <edward.c.wang@compdigitec.com>
2018-11-23 14:53:31 -0800

    Extract memories with ReplSeqMem
    This will make it easier to interface with downstream tools that deal
    with memories.

--- fpga/fpga.mk
+++ fpga/fpga.mk
@@ -20,10 +20,21 @@ $(VERILOG): $(SRC_DIR)/$(MODULE)/*.scala $(FIRRTL_JAR)
 	$(SBT) "project Core" "run $(CORE_CONFIG) --compiler high --target-dir $(SBT_TO_BASE)/$(FPGA_SRC_DIR)"
 	# high-firrtl is dumped into $(FPGA_SRC_DIR)/$(MODULE).hi.fir
 
+	# Replace SeqMem (currently only DSpm supported since it is a 1r1w port)
+	# See https://github.com/freechipsproject/firrtl/issues/856
+	java -jar $(FIRRTL_JAR) --compiler middle \
+		--repl-seq-mem -c:Core:-o:$(FPGA_SRC_DIR)/$(MODULE).conf -i $(FPGA_SRC_DIR)/$(MODULE).hi.fir \
+		-o $(FPGA_SRC_DIR)/$(MODULE).stripped_mems.mid.fir
+
 	# Use FIRRTL to compile to Verilog
 	java -jar $(FIRRTL_JAR) --compiler verilog \
 		--input-file $(FPGA_SRC_DIR)/$(MODULE).hi.fir \
 		--target-dir $(FPGA_SRC_DIR) \
+		--top-name $(MODULE)
+	java -jar $(FIRRTL_JAR) --compiler verilog \
+		--input-file $(FPGA_SRC_DIR)/$(MODULE).stripped_mems.mid.fir \
+		--target-dir $(FPGA_SRC_DIR) \
+		-o $(FPGA_SRC_DIR)/$(MODULE).stripped_mems.v \
 		--top-name $(MODULE)
 
 	# TODO: do these in FIRRTL or use the new memory-loading feature of Chisel
