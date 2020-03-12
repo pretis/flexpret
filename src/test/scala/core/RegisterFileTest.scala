36d1ca01f402fce9cfa4407b9847ec11d02842ea
Edward Wang <edward.c.wang@compdigitec.com>
2020-03-11 20:02:57 -0400

    Support a variable number of read/write ports

--- src/test/scala/core/RegisterFileTest.scala
+++ src/test/scala/core/RegisterFileTest.scala
@@ -16,7 +16,7 @@ import chiseltest.experimental.TestOptionBuilder._
 import Core.FlexpretConstants._
 import Core.FlexpretConfiguration
 
-import flexpret.core.{RegisterFile, RegisterFileReadIO}
+import flexpret.core.{RegisterFile, RegisterFileReadIO, RegisterFileWriteIO}
 
 class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
   behavior of "RegisterFile"
@@ -25,13 +25,13 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
   def registerFile = new RegisterFile(threads=threads)
 
   /* Write something to the register file */
-  def write(c: RegisterFile, thread: Int, addr: Int, data: UInt): Unit = {
+  def write(c: Module, w: RegisterFileWriteIO, thread: Int, addr: Int, data: UInt): Unit = {
     require(addr < 32)
     timescope {
-      c.io.rd.enable.poke(true.B)
-      c.io.rd.thread.poke(thread.U)
-      c.io.rd.addr.poke(addr.U)
-      c.io.rd.data.poke(data)
+      w.enable.poke(true.B)
+      w.thread.poke(thread.U)
+      w.addr.poke(addr.U)
+      w.data.poke(data)
       c.clock.step()
     }
   }
@@ -49,7 +49,7 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
 
   /* Write the given register data */
   def writeData(c: RegisterFile, thread: Int, addrDatas: Map[Int, String]): Unit = {
-    addrDatas.foreach { case (k, v) => write(c, thread, k, v.U) }
+    addrDatas.foreach { case (k, v) => write(c, c.io.write(0), thread, k, v.U) }
   }
 
   /* Test a thread */
@@ -57,44 +57,44 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
     writeData(c, thread, addrDatas)
 
     addrDatas.foreach { case (k, v) =>
-      assert(read(c, c.io.rs1, thread, k) == v.U.litValue)
-      assert(read(c, c.io.rs2, thread, k) == v.U.litValue)
+      assert(read(c, c.io.read(0), thread, k) == v.U.litValue)
+      assert(read(c, c.io.read(1), thread, k) == v.U.litValue)
     }
   }
 
   it should "read back what we read before even if we are reading 0 next" in {
-    test(registerFile) { c =>
+    test(registerFile).withAnnotations(Seq(treadle.WriteVcdAnnotation)) { c =>
       val thread = 0
       val addr = 31
       val data = "hf00df00d".U
-      write(c, thread, addr, data)
+      write(c, c.io.write(0), thread, addr, data)
 
       timescope {
         // The values we are going to check
-        c.io.rs1.thread.poke(thread.U)
-        c.io.rs1.addr.poke(addr.U)
-        c.io.rs2.thread.poke(thread.U)
-        c.io.rs2.addr.poke(0.U)
+        c.io.read(0).thread.poke(thread.U)
+        c.io.read(0).addr.poke(addr.U)
+        c.io.read(1).thread.poke(thread.U)
+        c.io.read(1).addr.poke(0.U)
         c.clock.step()
 
         // Values for the subsequent cycle, shouldn't interfere with the above
-        c.io.rs1.thread.poke(thread.U)
-        c.io.rs1.addr.poke(0.U)
-        c.io.rs2.thread.poke(thread.U)
-        c.io.rs2.addr.poke(addr.U)
+        c.io.read(0).thread.poke(thread.U)
+        c.io.read(0).addr.poke(0.U)
+        c.io.read(1).thread.poke(thread.U)
+        c.io.read(1).addr.poke(addr.U)
       }
 
       // Read data from first set of reads
-      c.io.rs1.data.expect(data)
-      c.io.rs2.data.expect(0.U)
+      c.io.read(0).data.expect(data)
+      c.io.read(1).data.expect(0.U)
     }
   }
 
   it should "write and read in 1 thread" in {
     test(registerFile) { c =>
       // Set a default register to read that is not 0 due to the bug above
-      c.io.rs1.addr.poke(31.U)
-      c.io.rs2.addr.poke(31.U)
+      c.io.read(0).addr.poke(31.U)
+      c.io.read(1).addr.poke(31.U)
 
       val addrDatas = Map(
         8 -> "h88887777",
@@ -109,8 +109,8 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
   it should "not cross-contaminate threads" in {
     test(registerFile) { c =>
       // Set a default register to read that is not 0 due to the bug above
-      c.io.rs1.addr.poke(31.U)
-      c.io.rs2.addr.poke(31.U)
+      c.io.read(0).addr.poke(31.U)
+      c.io.read(1).addr.poke(31.U)
 
       (0 until 3).foreach { thread =>
         val addrDatas = Map(
@@ -128,8 +128,8 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
   it should "operate read ports independently" in {
     test(registerFile) { c =>
       // Set a default register to read that is not 0 due to the bug above
-      c.io.rs1.addr.poke(31.U)
-      c.io.rs2.addr.poke(31.U)
+      c.io.read(0).addr.poke(31.U)
+      c.io.read(1).addr.poke(31.U)
 
       val thread = 0
       val addrDatas = Map(
@@ -147,9 +147,9 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
       // Read the data from the two ports in different orders
       val keys = addrDatas.keySet.toSeq
       fork {
-        keys.foreach { k => assert(read(c, c.io.rs1, thread, k) == addrDatas(k).U.litValue) }
+        keys.foreach { k => assert(read(c, c.io.read(0), thread, k) == addrDatas(k).U.litValue) }
       } .fork {
-        keys.reverse.foreach { k => assert(read(c, c.io.rs2, thread, k) == addrDatas(k).U.litValue) }
+        keys.reverse.foreach { k => assert(read(c, c.io.read(1), thread, k) == addrDatas(k).U.litValue) }
       } .join
     }
   }
@@ -160,10 +160,10 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
       val addr = 31
       val data = "hf00df00d".U
       fork {
-        write(c, thread, addr, data)
+        write(c, c.io.write(0), thread, addr, data)
       } .fork {
-        assert(read(c, c.io.rs1, thread, addr) == data.litValue)
-        assert(read(c, c.io.rs2, thread, addr) == data.litValue)
+        assert(read(c, c.io.read(0), thread, addr) == data.litValue)
+        assert(read(c, c.io.read(1), thread, addr) == data.litValue)
       } .join
     }
   }
@@ -174,33 +174,33 @@ class RegisterFileTest extends FlatSpec with ChiselScalatestTester {
       val data0 = "hf00df00d".U
       val data1 = "hf00df11d".U
       val data2 = "hf00df22d".U
-      write(c, thread=0, addr=addr, data0)
-      write(c, thread=1, addr=addr, data1)
-      write(c, thread=2, addr=addr, data2)
+      write(c, c.io.write(0), thread=0, addr=addr, data0)
+      write(c, c.io.write(0), thread=1, addr=addr, data1)
+      write(c, c.io.write(0), thread=2, addr=addr, data2)
 
       // Smoke test reading
-      assert(read(c, c.io.rs1, 0, addr) == data0.litValue)
-      assert(read(c, c.io.rs1, 1, addr) == data1.litValue)
-      assert(read(c, c.io.rs1, 2, addr) == data2.litValue)
+      assert(read(c, c.io.read(0), 0, addr) == data0.litValue)
+      assert(read(c, c.io.read(0), 1, addr) == data1.litValue)
+      assert(read(c, c.io.read(0), 2, addr) == data2.litValue)
 
       // Check that the invalid thread doesn't leak
       {
-        val read3 = read(c, c.io.rs1, 3, addr)
+        val read3 = read(c, c.io.read(0), 3, addr)
         assert(read3 != data0.litValue)
         assert(read3 != data1.litValue)
         assert(read3 != data2.litValue)
       }
       // Try writing to an invalid thread
-      write(c, thread=3, addr=addr, "haaaaaaaa".U)
+      write(c, c.io.write(0), thread=3, addr=addr, "haaaaaaaa".U)
 
       // Check that valid threads aren't affected
-      assert(read(c, c.io.rs1, 0, addr) == data0.litValue)
-      assert(read(c, c.io.rs1, 1, addr) == data1.litValue)
-      assert(read(c, c.io.rs1, 2, addr) == data2.litValue)
+      assert(read(c, c.io.read(0), 0, addr) == data0.litValue)
+      assert(read(c, c.io.read(0), 1, addr) == data1.litValue)
+      assert(read(c, c.io.read(0), 2, addr) == data2.litValue)
 
       // Check that the invalid thread still doesn't leak
       {
-        val read3 = read(c, c.io.rs1, 3, addr)
+        val read3 = read(c, c.io.read(0), 3, addr)
         assert(read3 != data0.litValue)
         assert(read3 != data1.litValue)
         assert(read3 != data2.litValue)
