5c159639d838b434bcf9f196fe61812dcfa762b0
Edward Wang <edward.c.wang@compdigitec.com>
2021-03-22 00:09:01 -0400

    Refactor out the imem simulator

---
+++ src/test/scala/core/ImemSimulator.scala
@@ -0,0 +1,59 @@
+/******************************************************************************
+Description: Test for a simple core.
+Author: Edward Wang <edwardw@eecs.berkeley.edu>
+Contributors:
+License: See LICENSE.txt
+******************************************************************************/
+package flexpret.core.test
+
+import org.scalatest._
+
+import chisel3._
+
+import chiseltest._
+import chiseltest.experimental.TestOptionBuilder._
+
+import Core.FlexpretConstants._
+
+import flexpret.core.Core
+//import flexpret.core.Datapath
+import flexpret.core.FlexpretConfiguration
+import flexpret.core.InstMemConfiguration
+import Core.Datapath
+import Core.InstMemCoreIO
+
+/**
+ * Simulate an instruction memory module on a InstMemCoreIO.
+ * @param prog Program memory as Chisel hex strings. Example:
+ * val prog: Seq[String] = scala.collection.immutable.Vector(
+ *   "h00100313", // li t1,1
+      "hcc431073", // csrw 0xcc4,t1
+      "h00000013", // nop
+      "hcc401073", // csrw 0xcc4,zero
+      "h00000013", // nop
+      "hfedff06f", // j 0
+    )
+ * @param defaultInstr Default instruction if out of bounds (as a Chisel hex str).
+ * @param startAddr imem entry to start instructions at (default 0).
+ *                  NOTE: this is not a byte address! e.g. 1 would correspond
+ *                  to instruction at PC+4.
+ */
+class ImemSimulator(
+  val prog: Seq[String],
+  val defaultInstr: String,
+  val clk: Clock, val memIO: InstMemCoreIO,
+  val startAddr: Int = 0) {
+  require(startAddr >= 0, "Start must be positive")
+
+  def sim(cycles: Int): Unit = {
+    var i = 0
+    while (i < cycles) { i += 1;
+      val addr = memIO.r.addr.peek().litValue.toInt
+      val enabled = memIO.r.enable.peek().litValue
+      clk.step()
+
+      val imemData = prog.lift.apply(addr - startAddr).getOrElse(defaultInstr)
+      memIO.r.data_out.poke(imemData.U)
+    }
+  }
+}
--- src/test/scala/core/SimpleCoreTest.scala
+++ src/test/scala/core/SimpleCoreTest.scala
@@ -31,35 +31,30 @@ class SimpleCoreTest extends FlatSpec with ChiselScalatestTester {
     dMemKB=256, mul=false, features="all")
   def core = new Core(conf)
 
-  def imemSimulator(cycles: Int, clk: Clock, memIO: InstMemCoreIO): Unit = {
-    var i = 0 // TODO: remove this hideous hack to timeout the imem sim
-    while (i < cycles) { i += 1;
-      val addr = memIO.r.addr.peek().litValue.toInt
-      val enabled = memIO.r.enable.peek().litValue
-      clk.step()
-
-      // Generate with ./scripts/parse_disasm.py
-      val prog: Seq[String] = scala.collection.immutable.Vector(
-        /* 0 */ "h00100313", // li t1,1
-        /* 4 */ "hcc431073", // csrw 0xcc4,t1
-        /* 8 */ "h00000013", // nop
-        /* c */ "hcc401073", // csrw 0xcc4,zero
-        /* 10 */ "h00000013", // nop
-        /* 14 */ "hfedff06f", // j 0
-      )
-
-      val imemData = prog.lift.apply(addr).getOrElse("h00000067") // jr x0
-      memIO.r.data_out.poke(imemData.U)
-    }
-  }
+  // Generate with ./scripts/parse_disasm.py
+  val prog: Seq[String] = scala.collection.immutable.Vector(
+    /* 0 */ "h00100313", // li t1,1
+    /* 4 */ "hcc431073", // csrw 0xcc4,t1
+    /* 8 */ "h00000013", // nop
+    /* c */ "hcc401073", // csrw 0xcc4,zero
+    /* 10 */ "h00000013", // nop
+    /* 14 */ "hfedff06f", // j 0
+  )
 
   it should "write a CSR GPIO" in {
     test(core).withAnnotations(Seq(treadle.WriteVcdAnnotation)) { c =>
+      val imemSim = new ImemSimulator(
+        prog=prog,
+        defaultInstr="h00000067", // jr x0
+        clk=c.clock,
+        memIO=c.io.imem_core.get
+      )
+
       var upTransitioned = false
       var downTransitioned = false
 
       fork {
-        imemSimulator(cycles=50, c.clock, c.io.imem_core)
+        imemSim.sim(cycles=50)
       } .fork {
         for (i <- 0 to 50) {
           val prev = c.io.gpio.out(0).peek.litValue
