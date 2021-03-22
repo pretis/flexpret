b705e9e2909e836cc7ab59ea665ae189a8797b6a
Edward Wang <edward.c.wang@compdigitec.com>
2021-03-22 02:17:20 -0400

    Add a toHost CSR test

--- src/test/scala/core/SimpleCoreTest.scala
+++ src/test/scala/core/SimpleCoreTest.scala
@@ -31,20 +31,18 @@ class SimpleCoreTest extends FlatSpec with ChiselScalatestTester {
     dMemKB=256, mul=false, features="all")
   def core = new Core(conf)
 
-  // Generate with ./scripts/parse_disasm.py
-  val prog: Seq[String] = scala.collection.immutable.Vector(
-    /* 0 */ "h00100313", // li t1,1
-    /* 4 */ "hcc431073", // csrw 0xcc4,t1
-    /* 8 */ "h00000013", // nop
-    /* c */ "hcc401073", // csrw 0xcc4,zero
-    /* 10 */ "h00000013", // nop
-    /* 14 */ "hfedff06f", // j 0
-  )
-
   it should "write a CSR GPIO" in {
     test(core).withAnnotations(Seq(treadle.WriteVcdAnnotation)) { c =>
       val imemSim = new ImemSimulator(
-        prog=prog,
+        // Generate with ./scripts/parse_disasm.py
+        prog=scala.collection.immutable.Vector(
+          /* 0 */ "h00100313", // li t1,1
+          /* 4 */ "hcc431073", // csrw 0xcc4,t1
+          /* 8 */ "h00000013", // nop
+          /* c */ "hcc401073", // csrw 0xcc4,zero
+          /* 10 */ "h00000013", // nop
+          /* 14 */ "hfedff06f", // j 0
+        ),
         defaultInstr="h00000067", // jr x0
         clk=c.clock,
         memIO=c.io.imem_core.get
@@ -67,6 +65,34 @@ class SimpleCoreTest extends FlatSpec with ChiselScalatestTester {
 
       assert(upTransitioned, "Must have transitioned up at least once")
       assert(downTransitioned, "Must have transitioned down at least once")
+    }
+  }
+
+  it should "write the toHost CSR" in {
+    test(core).withAnnotations(Seq(treadle.WriteVcdAnnotation)) { c =>
+      val imemSim = new ImemSimulator(
+        // Generate with ./scripts/parse_disasm.py
+        prog=scala.collection.immutable.Vector(
+          /* 0 */ "h054c43b7", // lui t2,0x54c4
+          /* 4 */ "h4dc38393", // addi t2,t2,1244 # li t2, 88884444
+          /* 8 */ "h51e39073", // csrw 0x51e,t2
+          /* c */ "h0000006f", // j c <loop>
+        ),
+        defaultInstr="h00000067", // jr x0
+        clk=c.clock,
+        memIO=c.io.imem_core.get
+      )
+
+      val cycles=25
+      fork {
+        imemSim.sim(cycles=cycles)
+      } .fork {
+        for (i <- 0 to cycles) {
+          c.clock.step()
+        }
+        val toHostVal = c.io.host.to_host.peek.litValue
+        assert(toHostVal == 88884444)
+      } .join
     }
   }
 }
