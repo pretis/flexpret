fd23b7e398bc82fbe9e1b5f6c61bef680e59fdd9
Edward Wang <edward.c.wang@compdigitec.com>
2021-04-21 00:31:28 -0400

    Split off ALU

---
+++ src/main/scala/Core/ALU.scala
@@ -0,0 +1,87 @@
+/******************************************************************************
+File: ALU.scala
+Description: ALU
+Author: Michael Zimmer (mzimmer@eecs.berkeley.edu)
+Author: Edward Wang (edwardw@eecs.berkeley.edu)
+Contributors:
+License: See LICENSE.txt
+******************************************************************************/
+package flexpret.core
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.ChiselEnum
+
+/**
+ * ALU operation types.
+ */
+object ALUTypes extends ChiselEnum {
+  val Add = Value
+  val Sub = Value
+  val ShiftLeft = Value
+  val ShiftRightArithmetic = Value
+  val ShiftRightLogical = Value
+  val And = Value
+  val Or = Value
+  val Xor = Value
+  val LessThanSigned = Value
+  val LessThanUnsigned = Value
+
+  val Unknown = BitPat("b" + "?" * ALUTypes.getWidth)
+
+  // Syntactic sugar
+  def unapply(x: UInt): Option[ALUTypes.Type] = Some(ALUTypes(x))
+}
+
+object ExampleEnum extends ChiselEnum {
+  val Foo, Bar = Value
+}
+
+/**
+ * Helpful BitPat aliases for ALU types. For use in a BitPat-based decoder.
+ */
+object ALUBitPats {
+  // Work around Chisel3 #1871
+  def toBitPat(aluLit: ALUTypes.Type) = BitPat(aluLit.litValue.U(ALUTypes.getWidth.W))
+
+  val ALU_X = ALUTypes.Unknown
+  val ALU_ADD = toBitPat(ALUTypes.Add)
+  val ALU_SUB = toBitPat(ALUTypes.Sub)
+  val ALU_SL = toBitPat(ALUTypes.ShiftLeft)
+  val ALU_SRA = toBitPat(ALUTypes.ShiftRightArithmetic)
+  val ALU_SRL = toBitPat(ALUTypes.ShiftRightLogical)
+  val ALU_AND = toBitPat(ALUTypes.And)
+  val ALU_OR = toBitPat(ALUTypes.Or)
+  val ALU_XOR = toBitPat(ALUTypes.Xor)
+  val ALU_LTS = toBitPat(ALUTypes.LessThanSigned)
+  val ALU_LTU = toBitPat(ALUTypes.LessThanUnsigned)
+}
+
+class ALU extends Module {
+  val io = IO(new Bundle {
+    val op1    = Input(UInt(32.W))
+    val op2    = Input(UInt(32.W))
+    val shift  = Input(UInt(4.W)) // used only for io.shifting operations
+    val func   = Input(ALUTypes())
+    val result = Output(UInt(32.W))
+  })
+
+  val dontCare = Wire(UInt(32.W))
+  dontCare := DontCare
+
+  import ALUTypes._
+  io.result := MuxLookup(io.func.asUInt, dontCare, Seq[(UInt, UInt)](
+    Add.asUInt -> (io.op1 + io.op2),
+    Sub.asUInt -> (io.op1 - io.op2),
+    ShiftLeft.asUInt -> (io.op1 << io.shift)(31, 0),
+    ShiftRightArithmetic.asUInt -> (io.op1.asSInt >> io.shift).asUInt,
+    ShiftRightLogical.asUInt -> (io.op1 >> io.shift),
+    And.asUInt -> (io.op1 & io.op2),
+    Or.asUInt -> (io.op1 | io.op2),
+    Xor.asUInt -> (io.op1 ^ io.op2),
+    LessThanSigned.asUInt -> (io.op1.asSInt < io.op2.asSInt).asUInt,
+    LessThanUnsigned.asUInt -> (io.op1 < io.op2),
+
+  ))
+}
+
--- src/main/scala/Core/constants.scala
+++ src/main/scala/Core/constants.scala
@@ -2,7 +2,7 @@
 File: constants.scala
 Description: Constant values for parameters, control signals, and RISC-V ISA.
 Author: Michael Zimmer (mzimmer@eecs.berkeley.edu)
-Contributors: 
+Contributors:
 License: See LICENSE.txt
 ******************************************************************************/
 package Core
@@ -22,7 +22,7 @@ object FlexpretConstants
   def Y = BitPat("b1")
   def N = BitPat("b0")
   val X = BitPat("b?")
-  
+
   // immediate
   val IMM_WI = 3
   val IMM_S  = UInt(0, 3)
@@ -39,7 +39,7 @@ object FlexpretConstants
   val OP1_RS1 = UInt(1, 2)
   val OP1_0   = UInt(2, 2)
   val OP1_X   = BitPat("b??")
- 
+
   // ALU op2 select
   val OP2_WI = 2
   val OP2_IMM = UInt(0, 2)
@@ -48,20 +48,6 @@ object FlexpretConstants
   val OP2_4   = UInt(3, 2) // only needed for WU
   val OP2_X   = BitPat("b??")
 
-  // ALU operation types
-  val ALU_WI = 4
-  val ALU_ADD  = UInt( 0, 4)
-  val ALU_SLL  = UInt( 1, 4)
-  val ALU_XOR  = UInt( 4, 4)
-  val ALU_SRL  = UInt( 5, 4)
-  val ALU_OR   = UInt( 6, 4)
-  val ALU_AND  = UInt( 7, 4)
-  val ALU_SUB  = UInt( 8, 4)
-  val ALU_SLT  = UInt(10, 4)
-  val ALU_SLTU = UInt(11, 4)
-  val ALU_SRA  = UInt(13, 4)
-  val ALU_X    = BitPat("b????")
-  
   // branch condition
   val BR_WI = 3
   val BR_EQ  = UInt(0, 3)
@@ -71,21 +57,21 @@ object FlexpretConstants
   val BR_LTU = UInt(4, 3)
   val BR_GEU = UInt(5, 3)
   val BR_X   = BitPat("b???")
- 
+
   // CSR types
   val CSR_WI = 2
   val CSR_W = UInt(1, 2)
   val CSR_S = UInt(2, 2)
   val CSR_C = UInt(3, 2)
   val CSR_X = BitPat("b??")
-  
+
   // rd from execute stage select
   val EXE_RD_WI = 2
   val EXE_RD_ALU = UInt(0, 2)
   val EXE_RD_CSR = UInt(1, 2)
   val EXE_RD_PC4 = UInt(2, 2)
   val EXE_RD_X   = BitPat("b??")
-  
+
   // memory load/store operation types
   val MEM_WI = 4
   val MEM_LB  = UInt(0,  4)
@@ -104,15 +90,15 @@ object FlexpretConstants
   val MUL_HSU = UInt(2, 2)
   val MUL_HU  = UInt(3, 2)
   val MUL_X   = BitPat("b??")
-  
+
   // rd from memory stage select
   val MEM_RD_WI = 2
   val MEM_RD_REG = UInt(0, 2)
   val MEM_RD_MEM = UInt(1, 2)
   val MEM_RD_MUL = UInt(2, 2)
   val MEM_RD_X   = BitPat("b??")
-  
-  
+
+
   // ************************************************************
   // Determined by control
 
@@ -136,10 +122,10 @@ object FlexpretConstants
   val RS2_EXE = UInt(1, 2)
   val RS2_MEM = UInt(2, 2)
   val RS2_WB  = UInt(3, 2)
- 
+
   // ************************************************************
   // Constants
-  
+
   // thread scheduling slots
   val SLOT_WI = 4
   val SLOT_T0 = UInt(0, 4)
@@ -178,7 +164,7 @@ object FlexpretConstants
   val ADDR_DSPM_VAL  = Bits("b001", ADDR_DSPM_BITS)
   val ADDR_BUS_BITS  = 2
   val ADDR_BUS_VAL   = Bits("b01", ADDR_BUS_BITS)
-  
+
   // memory protection
   val MEMP_WI = 4
   val MEMP_T0 = UInt(0, 4)
