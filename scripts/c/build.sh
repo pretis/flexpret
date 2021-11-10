b9f70d25f06f110be7df78bd6ed090df91045e59
Shaokai Lin <shaokai@berkeley.edu>
2021-11-09 18:32:35 -0800

    Restructure the scripts folder

---
+++ scripts/c/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A wrapper script that combines compilation amd parsing.
+# Authors:
+# - Edward Wang <edwardw@eecs.berkeley.edu>
+# - Shaokai Lin <shaokai@eecs.berkeley.edu>
+
+set -euxo pipefail
+
+SCRIPT_DIR=$(dirname "$0")
+
+$SCRIPT_DIR/compile.sh $1 $2
+$SCRIPT_DIR/parse_disasm.py $1.dump.txt readmemh > imem.hex.txt
---
+++ scripts/c/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Compile a RISC-V C program with a start script and create an objdump.
+# Authors:
+# - Edward Wang <edwardw@eecs.berkeley.edu>
+# - Shaokai Lin <shaokai@eecs.berkeley.edu>
+
+set -ex
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$0")
+LIB_DIR=$SCRIPT_DIR/../../programs/lib
+
+BIN="$1"
+shift
+
+riscv64-unknown-elf-gcc -I$LIB_DIR/include -g -static -O0 -march=rv32i -mabi=ilp32 -specs=nosys.specs -nostartfiles -Wl,-Ttext=0x00000000 -o "$BIN" $LIB_DIR/start.S "$@"
+riscv64-unknown-elf-objdump -S -d "$BIN" > "$BIN.dump.txt"
