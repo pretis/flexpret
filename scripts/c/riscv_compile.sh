9bba6ce59ed499fbf36942cbda2af06995a63bf4
Shaokai Lin <shaokai@berkeley.edu>
2022-08-27 02:19:54 -0700

    Update README and scripts

--- scripts/c/riscv_compile.sh
+++ scripts/c/riscv_compile.sh
@@ -10,8 +10,7 @@ set -euo pipefail
 SCRIPT_DIR=$(dirname "$0")
 LIB_DIR=$SCRIPT_DIR/../../programs/lib
 LINKER_SCRIPT=$SCRIPT_DIR/../../programs/lib/linker-scripts/flexpret.ld
-# CC=riscv64-unknown-elf-gcc
-CC=riscv-none-embed-gcc
+CC=riscv32-unknown-elf-gcc
 
 $CC -I$LIB_DIR/include -T $LINKER_SCRIPT -Xlinker -Map=output.map -g -static -O0 -march=rv32i -mabi=ilp32 -nostartfiles --specs=nosys.specs -o $1 $LIB_DIR/start.S "${@:2}"
 riscv64-unknown-elf-objdump -S -d $1 > $1.dump.txt
