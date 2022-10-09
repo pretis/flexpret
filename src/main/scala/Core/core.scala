0862ef7b6b3cc66dd50be9e4551892febe892099
Shaokai Lin <shaokai@berkeley.edu>
2022-10-08 23:10:06 -0700

    Slight formatting

--- src/main/scala/Core/core.scala
+++ src/main/scala/Core/core.scala
@@ -55,12 +55,12 @@ case class FlexpretConfiguration(threads: Int, flex: Boolean,
     else (mt, mt, true, true, true, List(0, 1, 2, 3, 6, 8, 9, 10, 11))
 
   // Design Space Exploration
-  val regBrJmp = mt && !flex // delay B*, J* 1 cycle to reduce timing path
-  val regEvec = true // delay trapping 1 cycle to reduce timing path
-  val regSchedule = true // delay DU, WU and schedule update 1 cycle to reduce timing path
-  val dedicatedCsrData = true // otherwise wait for pass through ALU
-  val iMemCoreRW = true // 'true' required for load/store to ISPM
-  val privilegedMode = false // Off until updated to latest compiler..
+  val regBrJmp          = mt && !flex // delay B*, J* 1 cycle to reduce timing path
+  val regEvec           = true // delay trapping 1 cycle to reduce timing path
+  val regSchedule       = true // delay DU, WU and schedule update 1 cycle to reduce timing path
+  val dedicatedCsrData  = true // otherwise wait for pass through ALU
+  val iMemCoreRW        = true // 'true' required for load/store to ISPM
+  val privilegedMode    = false // Off until updated to latest compiler..
   //val privilegedMode = true
 
   // ************************************************************
@@ -68,63 +68,63 @@ case class FlexpretConfiguration(threads: Int, flex: Boolean,
   // General
   // TODO(edwardw): test this assumption and remove the use of the deprecated log2Up
   //require(threads > 0, "Cannot have zero hardware threads")
-  val threadBits = Chisel.log2Up(threads)
+  val threadBits    = Chisel.log2Up(threads)
 
   // Datapath
   // If true, allow arbitrary interleaving of threads in pipeline using bypass paths
   // If false, at least four hardware threads must be interleaved in the
   // pipeline (no control unit support for other options)
-  val bypassing = (threads < 4) || flex
+  val bypassing     = (threads < 4) || flex
 
   // Scheduler
-  val roundRobin = !flex
-  val initialSlots = List(
+  val roundRobin    = !flex
+  val initialSlots  = List(
     SLOT_D, SLOT_D, SLOT_D, SLOT_D, SLOT_D, SLOT_D, SLOT_D, SLOT_T0
   )
   val initialTmodes = (0 until threads).map(i => if (i != 0) TMODE_HZ else TMODE_HA)
 
   // I-Spm
-  val iMemDepth = 256 * imemConfig.sizeKB     // 32-bit entries
-  val iMemAddrBits = log2Ceil(4 * iMemDepth)  // word addressable
+  val iMemDepth     = 256 * imemConfig.sizeKB   // 32-bit entries
+  val iMemAddrBits  = log2Ceil(4 * iMemDepth)   // word addressable
   val iMemHighIndex = log2Ceil(4 * iMemDepth) - 1
-  val iMemForceEn = false
-  val iMemBusRW = false
+  val iMemForceEn   = false
+  val iMemBusRW     = false
 
   // D-Spm
-  val dMemDepth = 256 * dMemKB                // 32-bit entries
-  val dMemAddrBits = log2Ceil(4 * dMemDepth)  // byte addressable
+  val dMemDepth     = 256 * dMemKB              // 32-bit entries
+  val dMemAddrBits  = log2Ceil(4 * dMemDepth)   // byte addressable
   val dMemHighIndex = log2Ceil(4 * dMemDepth) - 1
-  val dMemForceEn = false
-  val dMemBusRW = false
+  val dMemForceEn   = false
+  val dMemBusRW     = false
 
   // GPIO
-  val gpiPortSizes = List(1, 1, 1, 1)
-  val gpoPortSizes = List(2, 2, 2, 2)
-  val initialGpo = List(
+  val gpiPortSizes  = List(1, 1, 1, 1)
+  val gpoPortSizes  = List(2, 2, 2, 2)
+  val initialGpo    = List(
     MEMP_T0, MEMP_SH, MEMP_SH, MEMP_SH
   )
 
   // Bus
   // upper bits are for thread ID
-  val busAddrBits = 10
+  val busAddrBits   = 10
 
   // Memory Protection
-  val memRegions = 8
-  val iMemLowIndex = iMemHighIndex - log2Ceil(memRegions) + 1
-  val dMemLowIndex = dMemHighIndex - log2Ceil(memRegions) + 1
+  val memRegions    = 8
+  val iMemLowIndex  = iMemHighIndex - log2Ceil(memRegions) + 1
+  val dMemLowIndex  = dMemHighIndex - log2Ceil(memRegions) + 1
   // regions 0..7 (opposite of csr register format)
-  val initialIMem = List(
+  val initialIMem   = List(
     MEMP_SH, MEMP_RO, MEMP_RO, MEMP_RO, MEMP_RO, MEMP_RO, MEMP_RO, MEMP_RO
   )
-  val initialDMem = List(
+  val initialDMem   = List(
     MEMP_SH, MEMP_SH, MEMP_SH, MEMP_SH, MEMP_SH, MEMP_SH, MEMP_SH, MEMP_SH
   )
 
   // functionality
-  val timeBits = 32
-  val timeInc = 10
+  val timeBits      = 32
+  val timeInc       = 10
   require(timeBits <= 32)
-  val getTime = delayUntil || interruptExpire
+  val getTime       = delayUntil || interruptExpire
 
   // TODO: priv fault without loadstore
   // Supported exceptions
