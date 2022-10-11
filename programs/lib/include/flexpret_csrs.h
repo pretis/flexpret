8f3d3f1454e282db40c8e1dd3cf375b39e86e85e
Shaokai Lin <shaokai@berkeley.edu>
2022-10-10 22:16:10 -0700

    Update flexpret_csr.h and add a gpio test.

--- programs/lib/include/flexpret_csrs.h
+++ programs/lib/include/flexpret_csrs.h
@@ -1,27 +1,27 @@
 #ifndef FLEXPRET_CSRS_H
 #define FLEXPRET_CSRS_H
 
-#define CSR_TOHOST 0x51e
-#define CSR_FROMHOST 0x51f
-#define CSR_CYCLE 0xc00
-#define CSR_TIME 0xc01
-#define CSR_INSTRET 0xc02
-#define CSR_UARCH0 0xcc0
-#define CSR_UARCH1 0xcc1
-#define CSR_UARCH2 0xcc2
-#define CSR_UARCH3 0xcc3
-#define CSR_UARCH4 0xcc4
-#define CSR_UARCH5 0xcc5
-#define CSR_UARCH6 0xcc6
-#define CSR_UARCH7 0xcc7
-#define CSR_UARCH8 0xcc8
-#define CSR_UARCH9 0xcc9
-#define CSR_UARCH10 0xcca
-#define CSR_UARCH11 0xccb
-#define CSR_UARCH12 0xccc
-#define CSR_UARCH13 0xccd
-#define CSR_UARCH14 0xcce
-#define CSR_UARCH15 0xccf
+#define CSR_TOHOST      0x51e
+#define CSR_FROMHOST    0x51f
+#define CSR_CYCLE       0xc00
+#define CSR_TIME        0xc01
+#define CSR_INSTRET     0xc02
+#define CSR_UARCH0      0xcc0
+#define CSR_UARCH1      0xcc1
+#define CSR_UARCH2      0xcc2
+#define CSR_UARCH3      0xcc3
+#define CSR_UARCH4      0xcc4
+#define CSR_UARCH5      0xcc5
+#define CSR_UARCH6      0xcc6
+#define CSR_UARCH7      0xcc7
+#define CSR_UARCH8      0xcc8
+#define CSR_UARCH9      0xcc9
+#define CSR_UARCH10     0xcca
+#define CSR_UARCH11     0xccb
+#define CSR_UARCH12     0xccc
+#define CSR_UARCH13     0xccd
+#define CSR_UARCH14     0xcce
+#define CSR_UARCH15     0xccf
 
 #ifdef __ASSEMBLY__
 #define __ASM_STR(x)	x
@@ -39,21 +39,21 @@
   asm volatile ("csrw " __ASM_STR(reg) ", %0" :: "r"(val))
 
 #define swap_csr(reg, val) ({ long __tmp; \
-  asm volatile ("csrrw %0, " #reg ", %1" : "=r"(__tmp) : "r"(val)); \
+  asm volatile ("csrrw %0, " __ASM_STR(reg) ", %1" : "=r"(__tmp) : "r"(val)); \
   __tmp; })
 
 #define set_csr(reg, bit) ({ long __tmp; \
   if (__builtin_constant_p(bit) && (bit) < 32) \
-    asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
+    asm volatile ("csrrs %0, " __ASM_STR(reg) ", %1" : "=r"(__tmp) : "i"(bit)); \
   else \
-    asm volatile ("csrrs %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
+    asm volatile ("csrrs %0, " __ASM_STR(reg) ", %1" : "=r"(__tmp) : "r"(bit)); \
   __tmp; })
 
 #define clear_csr(reg, bit) ({ long __tmp; \
   if (__builtin_constant_p(bit) && (bit) < 32) \
-    asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "i"(bit)); \
+    asm volatile ("csrrc %0, " __ASM_STR(reg) ", %1" : "=r"(__tmp) : "i"(bit)); \
   else \
-    asm volatile ("csrrc %0, " #reg ", %1" : "=r"(__tmp) : "r"(bit)); \
+    asm volatile ("csrrc %0, " __ASM_STR(reg) ", %1" : "=r"(__tmp) : "r"(bit)); \
   __tmp; })
 
 #define rdtime() ({ unsigned long __tmp; \
