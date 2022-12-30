9b25126463d7213afe17ba38fd45cc86f7a2f825
Shaokai Lin <shaokai_lin@126.com>
2022-12-30 16:07:04 +0800

    Support multithreading in the startup code, update the compile script.

--- programs/lib/startup.c
+++ programs/lib/startup.c
@@ -1,11 +1,24 @@
+/**
+ * FlexPRET's startup code in C
+ * 
+ * Authors:
+ * - Shaokai Lin
+ * - Samuel Berkun
+ */
+
 #include <unistd.h>      // Declares _exit() with definition in syscalls.c.
 #include <stdint.h>
 #include <flexpret_io.h>
+#include <flexpret_lock.h>
 #include "tinyalloc/tinyalloc.h"
 
-#define DSPM_LIMIT          ((void*)0x2003E800) // 0x3E800 = 256K
+#define DSPM_LIMIT          ((void*)0x20040000) // 0x40000 = 256K
 #define TA_MAX_HEAP_BLOCK   1000
 #define TA_ALIGNMENT        4
+
+#ifndef NUM_THREADS
+#define NUM_THREADS 1
+#endif
 
 extern uint32_t __etext;
 extern uint32_t __data_start__;
@@ -13,6 +26,9 @@ extern uint32_t __data_end__;
 extern uint32_t __bss_start__;
 extern uint32_t __bss_end__;
 extern uint32_t end;
+
+static uint32_t __ready__    = 0; // FIXME: Replace by a condition variable.
+static uint32_t __num_done__ = 0; // FIXME: Replace by a condition variable.
 
 //prototype of main
 int main(void);
@@ -52,35 +68,64 @@ void free(void *ptr) {
  * Initialize initialized global variables, set uninitialized global variables
  * to zero, configure tinyalloc, and jump to main.
  */
-void Reset_Handler(void) {
-    // Copy .data section into the RAM
-    uint32_t size   = &__data_end__ - &__data_start__;
-    uint32_t *pDst  = (uint32_t*)&__data_start__;       // RAM
-    uint32_t *pSrc  = (uint32_t*)&__etext;              // ROM
+void Reset_Handler(uint32_t hartid) {
+    // Only thread 0 performs the setup,
+    // the other threads busy wait until ready.
+    if (hartid == 0) {
+        // Copy .data section into the RAM
+        uint32_t size   = &__data_end__ - &__data_start__;
+        uint32_t *pDst  = (uint32_t*)&__data_start__;       // RAM
+        uint32_t *pSrc  = (uint32_t*)&__etext;              // ROM
 
-    for (uint32_t i = 0; i < size; i++) {
-        *pDst++ = *pSrc++;
+        for (uint32_t i = 0; i < size; i++) {
+            *pDst++ = *pSrc++;
+        }
+
+        // Init. the .bss section to zero in RAM
+        size = (uint32_t)&__bss_end__ - (uint32_t)&__bss_start__;
+        pDst = (uint32_t*)&__bss_start__;
+        for(uint32_t i = 0; i < size; i++) {
+            *pDst++ = 0;
+        }
+
+        // Initialize tinyalloc.
+        ta_init( 
+            &end, // start of the heap space
+            DSPM_LIMIT,
+            TA_MAX_HEAP_BLOCK, 
+            16, // split_thresh: 16 bytes (Only used when reusing blocks.)
+            TA_ALIGNMENT
+        );
+
+        // Signal ready.
+        lock_acquire();
+        __ready__ = 1;
+        lock_release();
+    } else {
+        // Wait for thread 0 to finish setup.
+        // FIXME: Use delay until (DU)
+        // for precise synchronization.
+        while (__ready__ != 1);
     }
-
-    // Init. the .bss section to zero in RAM
-    size = (uint32_t)&__bss_end__ - (uint32_t)&__bss_start__;
-    pDst = (uint32_t*)&__bss_start__;
-    for(uint32_t i = 0; i < size; i++) {
-        *pDst++ = 0;
-    }
-
-    // Initialize tinyalloc.
-    ta_init( 
-        &end, // start of the heap space
-        DSPM_LIMIT,
-        TA_MAX_HEAP_BLOCK, 
-        16, // split_thresh: 16 bytes (Only used when reusing blocks.)
-        TA_ALIGNMENT
-    );
 
     // Call main().
     main();
 
     // Exit by calling the _exit() syscall.
-    _exit(0);
+    if (hartid == 0) {
+        lock_acquire();
+        __num_done__ += 1;
+        lock_release();
+
+        // Wait for all the threads to finish.
+        while (__num_done__ < NUM_THREADS);
+
+        // Exit the program.
+        _exit(0);
+    } else {
+        lock_acquire();
+        __num_done__ += 1;
+        lock_release();
+        while (1);
+    }
 }
