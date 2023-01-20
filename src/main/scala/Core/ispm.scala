a7122eab4266bf5fd1a793cb7de3889ae3f4b782
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2023-01-20 14:41:51 +0100

    Add Verilog file and use BlackBox to get a TDP for ISPM

--- src/main/scala/Core/ispm.scala
+++ src/main/scala/Core/ispm.scala
@@ -8,6 +8,8 @@ License: See LICENSE.txt
 package flexpret.core
 
 import chisel3._
+import chisel3.util._
+import _root_.dataclass.data
 
 class InstMemCoreIO(implicit conf: FlexpretConfiguration) extends Bundle {
   val r = new Bundle {
@@ -26,6 +28,22 @@ class InstMemCoreIO(implicit conf: FlexpretConfiguration) extends Bundle {
   }
 }
 
+class TrueDualPortBram(addrBits: Int, dataBits: Int) 
+extends BlackBox(Map("DATA" -> dataBits, "ADDR" -> addrBits)) {
+  val io = IO(new Bundle {
+    val clk = Input(Clock())
+    val a_wr = Input(Bool())
+    val a_addr = Input(UInt(addrBits.W))
+    val a_din = Input(UInt(dataBits.W))
+    val a_dout = Output(UInt(dataBits.W))
+    
+    val b_wr = Input(Bool())
+    val b_addr = Input(UInt(addrBits.W))
+    val b_din = Input(UInt(dataBits.W))
+    val b_dout = Output(UInt(dataBits.W))
+  })
+}
+
 class ISpm(implicit conf: FlexpretConfiguration) extends Module {
   val io = IO(new Bundle {
     val core = new InstMemCoreIO()
@@ -37,21 +55,31 @@ class ISpm(implicit conf: FlexpretConfiguration) extends Module {
 
   // memory for instruction SPM
   // sync read, sync write
-  val ispm = SyncReadMem(conf.iMemDepth, UInt(32.W))
+  val ispm = Module(new TrueDualPortBram(conf.iMemDepth, 32))
+
+
+
 
   // read port
-  io.core.r.data_out := ispm.read(io.core.r.addr, io.core.r.enable)
+  ispm.io.a_addr := io.core.r.addr
+  ispm.io.a_wr := false.B
+  ispm.io.a_din := 0.U
+  io.core.r.data_out := ispm.io.a_dout
 
+  // Second read/write port
   if (conf.iMemCoreRW || conf.iMemBusRW) {
-    // read/write port
-    val busRwPort = ispm(io.bus.addr)
+    // Drive default
+    ispm.io.b_wr := false.B
+    ispm.io.b_addr := 0.U
+    ispm.io.b_din := 0.U
 
     if (conf.iMemBusRW) {
-      io.bus.data_out := busRwPort
+      ispm.io.b_addr := io.bus.addr
+      io.bus.data_out := ispm.io.b_dout
       io.bus.ready := true.B
       when(io.bus.enable) {
         when(io.bus.write) {
-          busRwPort := io.bus.data_in
+          ispm.io.b_din := io.bus.data_in
         }
       }
     } else {
@@ -59,13 +87,14 @@ class ISpm(implicit conf: FlexpretConfiguration) extends Module {
     }
 
     // Core has priority over bus
-    val coreRwPort = ispm(io.core.rw.addr)
     if (conf.iMemCoreRW) {
-      io.core.rw.data_out := coreRwPort
+      ispm.io.b_addr := io.core.rw.addr
+      io.core.rw.data_out := ispm.io.b_dout
       when(io.core.rw.enable) {
         if(conf.iMemBusRW) io.bus.ready := false.B
         when(io.core.rw.write) {
-          coreRwPort := io.core.rw.data_in
+          ispm.io.b_wr := true.B
+          ispm.io.b_din := io.core.rw.data_in
         }
       }
     }
