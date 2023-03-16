5c83a6f3c583efa9548cb53483c02e4246563f7c
erlingrj <erling.jellum@gmail.com>
2023-03-16 09:35:28 +0100

    More trimming

--- programs/lib/include/flexpret.h
+++ programs/lib/include/flexpret.h
@@ -6,6 +6,9 @@
 #include "flexpret_io.h"
 #include "flexpret_lock.h"
 #include "flexpret_thread.h"
+#include "flexpret_assert.h"
+#include "flexpret_types.h"
+
 
 #include <stdbool.h>
 #include <stdint.h>
--- programs/lib/include/flexpret_stdio.h
+++
@@ -1,7 +0,0 @@
-#ifndef FLEXPRET_STDIO_H
-#define FLEXPRET_STDIO_H
-
-void print_str(const char *str);
-
-void print_int(int val);
-#endif
