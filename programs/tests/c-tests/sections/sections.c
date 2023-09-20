e18a44f1f62f633d457707a9fd7b1138ad6bdb06
Magnus Mæhlum <magnusmaehlum@outlook.com>
2023-09-20 19:59:53 +0200

    Replace .rodata.* -> .rodata* and .srodata.* -> .srodata* in flexpret.ld, which solves a lot of issues. Now, this looks the same as .text*. I suspect the reason this works is because the wildcard pattern .rodata.* does not actually match .rodata itself. And the same goes for .srodata.
    Also update sections test in c-tests folder with more sections tested. To view the results of the changes to the linker script, try to remove the changes and run the sections test.
    
    Furthermore, some multithreading tests failed before this change was made to the linker script. They seem to pass now. Try to run them all with and without the changes to the linker script.
    
    Note that the bug described in issue 78 has not been solved yet.

--- programs/tests/c-tests/sections/sections.c
+++ programs/tests/c-tests/sections/sections.c
@@ -1,18 +1,57 @@
 #include "flexpret.h"
 #include <stdlib.h>
+#include <stdint.h>
 
-// Adding '#' after the sections removes the warning; see
+#define TEXTDATA_INIT_VAL (41)
+#define RODATA_INIT_VAL   (42)
+#define SRODATA_INIT_VAL  (43)
+
+// Adding '#' after the sections removes an assembler warning; see
 // https://stackoverflow.com/questions/58455300/assembler-warning-with-gcc-warning-when-placing-data-in-text
-const int srodata __attribute__((section(".text#"))) = 42;
-int rodata __attribute__((section(".rodata#"))) = 43;
+const int32_t c_textdata __attribute__((section(".text.c#")))   = TEXTDATA_INIT_VAL;
+const int32_t c_rodata __attribute__((section(".rodata.c#")))   = RODATA_INIT_VAL;
+const int32_t c_srodata __attribute__((section(".srodata.c#"))) = SRODATA_INIT_VAL;
 
+int32_t d_textdata __attribute__((section(".text.d#")))         = TEXTDATA_INIT_VAL;
+int32_t d_rodata __attribute__((section(".rodata.d#")))         = RODATA_INIT_VAL;
+int32_t d_srodata __attribute__((section(".srodata.d#")))       = SRODATA_INIT_VAL;
+
+int32_t textdata __attribute__((section(".text#")))             = TEXTDATA_INIT_VAL;
+int32_t rodata __attribute__((section(".rodata#")))             = RODATA_INIT_VAL;
+int32_t srodata __attribute__((section(".srodata#")))           = SRODATA_INIT_VAL;
+
+
+// Pass-by-value will copy it, losing the address
+static inline void _print_addr_val(const int32_t *val)
+{
+    _fp_print((int) val);
+    _fp_print(*val);
+}
 
 int main() {
-    _fp_print(&srodata);
-    _fp_print(&rodata);
-    _fp_print(srodata);
-    _fp_print(rodata);
+    _print_addr_val(&c_textdata);
+    _print_addr_val(&c_rodata);
+    _print_addr_val(&c_srodata);
 
-    _fp_print(srodata);
+    _print_addr_val(&d_textdata);
+    _print_addr_val(&d_rodata);
+    _print_addr_val(&d_srodata);
+
+    _print_addr_val(&textdata);
+    _print_addr_val(&rodata);
+    _print_addr_val(&srodata);
+
+    assert(c_textdata == TEXTDATA_INIT_VAL);
+    assert(c_rodata   == RODATA_INIT_VAL);
+    assert(c_srodata  == SRODATA_INIT_VAL);
+
+    assert(d_textdata == TEXTDATA_INIT_VAL);
+    assert(d_rodata   == RODATA_INIT_VAL);
+    assert(d_srodata  == SRODATA_INIT_VAL);
+
+    assert(textdata == TEXTDATA_INIT_VAL);
+    assert(rodata   == RODATA_INIT_VAL);
+    assert(srodata  == SRODATA_INIT_VAL);
+    
     return 0;
 }
