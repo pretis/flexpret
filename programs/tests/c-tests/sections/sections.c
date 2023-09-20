1554ddec4ed24470ac527b1211ad4d512b244b39
Magnus Mæhlum <magnusmaehlum@outlook.com>
2023-09-20 18:50:21 +0200

    Remove warning in c-tests/sections

--- programs/tests/c-tests/sections/sections.c
+++ programs/tests/c-tests/sections/sections.c
@@ -1,8 +1,10 @@
 #include "flexpret.h"
 #include <stdlib.h>
 
-const int srodata __attribute__((section(".text"))) = 42;
-int rodata __attribute__((section(".rodata"))) = 43;
+// Adding '#' after the sections removes the warning; see
+// https://stackoverflow.com/questions/58455300/assembler-warning-with-gcc-warning-when-placing-data-in-text
+const int srodata __attribute__((section(".text#"))) = 42;
+int rodata __attribute__((section(".rodata#"))) = 43;
 
 
 int main() {
