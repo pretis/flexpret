4ebfd8090482385cff8afad27284fa5ecc03eee1
Magnus Mæhlum <magnusmaehlum@outlook.com>
2023-09-26 00:36:40 +0200

    Make changes to syscall.c to integrate with newlib. User can now call e.g. exit(), which will call newlib's implementation of _exit_r() along with our own _exit() in syscalls.c.
    Also add support for using errno in user programs, which is set by the syscalls.
    
    The implementation should be thread-safe in theory, but this has not yet been tested.

--- programs/lib/syscalls.c
+++ programs/lib/syscalls.c
@@ -3,51 +3,113 @@
 // https://github.com/riscvarchive/riscv-newlib/tree/riscv-newlib-3.2.0/libgloss/libnosys.
 #include <stdint.h>
 #include <errno.h>      // Defines ENOSYS.
-#include <sys/stat.h>   // Defines struct stat.
+#include <sys/time.h>
+
 #include <flexpret_io.h>
+#include <flexpret_csrs.h>
+#include <flexpret_config.h>
+#include <flexpret_time.h>
 
-int errno;
+// See https://github.com/eblot/newlib/blob/master/newlib/libc/include/reent.h
+// for more information on re-entry to newlib.
+#define __DYNAMIC_REENT__
+#define _REENT_SMALL
+#include <reent.h>
 
-int _close(int fildes) {
+// FIXME: Need to provide file descriptors to _reents variable
+// __FILE __sf[3];
+
+struct _reent _reents[NUM_THREADS] = { 0 };
+
+struct _reent *__getreent(void) {
+    uint32_t hartid = read_hartid();
+    return &_reents[hartid];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper functions
+////////////////////////////////////////////////////////////////////////////////
+
+static inline const uint64_t ns_to_s(const uint64_t ns) {
+    return ((uint64_t) (ns) / (uint64_t) (1e9));
+}
+
+static inline const uint64_t ns_to_us(const uint64_t ns) {
+    return ((uint64_t) (ns) / (uint64_t) (1e3));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Syscall implementations
+////////////////////////////////////////////////////////////////////////////////
+
+int _close (int fd) {
     errno = ENOSYS;
     return -1;
 }
 
-// FIXME: Does this also work on the FPGA?
-void _exit(int rc) {
-    _fp_finish();
-    while(1) {}
-    __builtin_unreachable();
-}
-
-int _fstat(int fildes, struct stat *st) {
+int _execve (const char *, char *const *, char *const *) {
     errno = ENOSYS;
     return -1;
 }
 
-int _getpid(void){
+int _fcntl (int, int, int) {
     errno = ENOSYS;
     return -1;
 }
 
-int _isatty(int file) {
-    errno = ENOSYS;
-    return 0;
-}
-
-int _kill(int pid, int sig) {
+int _fork (void) {
     errno = ENOSYS;
     return -1;
 }
 
-int _lseek(int file, int ptr, int dir) {
+int _fstat (int, struct stat *) {
     errno = ENOSYS;
     return -1;
 }
 
-int _read (int file, char *ptr, int len) {
-  	errno = ENOSYS;
-  	return -1;
+int _getpid (void) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _isatty (int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _kill (int, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _link (const char *, const char *) {
+    errno = ENOSYS;
+    return -1;
+}
+
+_off_t _lseek (int, _off_t, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _mkdir (const char *, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _open (const char *, int, int) {
+    errno = ENOSYS;
+    return -1;
+}
+
+_ssize_t _read (int, void *, size_t) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _rename (const char *, const char *) {
+    errno = ENOSYS;
+    return -1;
 }
 
 // FIXME: This should not be actually called because of tinyalloc.
@@ -65,7 +127,41 @@ void *_sbrk(int incr) {
    return (void *) prev_heap_end;
 }
 
-int _write (int file, char *ptr, int len) {
-  	errno = ENOSYS;
-  	return -1;
+int _stat (const char *, struct stat *) {
+    errno = ENOSYS;
+    return -1;
+}
+
+_CLOCK_T_ _times (struct tms *) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _unlink (const char *) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _wait (int *) {
+    errno = ENOSYS;
+    return -1;
+}
+
+_ssize_t _write (int, const void *, size_t) {
+    errno = ENOSYS;
+    return -1;
+}
+
+int _gettimeofday(struct timeval *__tp, void *__tzp) {
+    errno = 0;
+    uint64_t ns = rdtime64();
+    __tp->tv_sec  = ns_to_s(ns);
+    __tp->tv_usec = ns_to_us(ns % (int) (1e9));
+    return 0;
+}
+
+void _exit (int) {
+    _fp_finish();
+    while(1) {}
+    __builtin_unreachable();
 }
--- programs/tests/c-tests/syscall/syscall.c
+++ programs/tests/c-tests/syscall/syscall.c
@@ -1,5 +1,25 @@
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <errno.h>
+
+#include <flexpret.h>
 
 int main() {
-    _exit(1);
+    int ret = close(28);
+    _fp_print(errno);
+
+    int pid = getpid();
+    _fp_print(errno);
+
+    struct timeval tv;
+    ret = gettimeofday(&tv, NULL);
+    _fp_print(errno);
+    _fp_print(tv.tv_sec);
+    _fp_print(tv.tv_usec);
+
+    exit(1);
+
+    // Should not be printed
+    _fp_print(1);
 }
