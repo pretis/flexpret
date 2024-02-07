c68e5ca4249ee4054099990ad150d90bfda96d66
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-02-07 02:03:32 +0100

    Refactor interrupt test and begin work on multi-threaded interrupt test. But FlexPRET currently does not have support for multi-threaded IE (only one signal shared by all threads for when an IE occurs).

--- programs/tests/c-tests/interrupt/interrupt.c
+++ programs/tests/c-tests/interrupt/interrupt.c
@@ -1,47 +1,67 @@
 #include <flexpret.h>
 #include <errno.h>
+#include <string.h>
+
+#include "interrupt.h"
 
 // An interrupt will take time proportional to the number of threads
 // because more threads -> more wasted cycles in the pipeline
 // (At least when the hw threads are not sleeping, which is the case
 //  at the time of writing this test.)
 #define EXPIRE_DELAY_NS (((uint32_t)(1e4)) * (NUM_THREADS))
+#define TIMEOUT_INIT (10000)
 
-static volatile int flag0 = 0;
-static volatile int flag1 = 0;
-static volatile int ext_int_flag = 0;
-static volatile int du_int_triggered = 0;
-static volatile int wu_int_triggered = 0;
+static volatile int flag0[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
+static volatile int flag1[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
+static volatile int ext_int_flag[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
+static volatile int du_int_triggered[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
+static volatile int wu_int_triggered[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
+static volatile uint64_t isr_time[NUM_THREADS] = THREAD_ARRAY_INITIALIZER(0);
 
-static uint64_t isr_time = 0;
+void reset_flags(void) {
+    memset((void *) flag0, 0, sizeof(flag0));
+    memset((void *) flag1, 0, sizeof(flag1));
+    memset((void *) ext_int_flag, 0, sizeof(ext_int_flag));
+    memset((void *) du_int_triggered, 0, sizeof(du_int_triggered));
+    memset((void *) wu_int_triggered, 0, sizeof(wu_int_triggered));
+}
 
 void ie_isr0(void) {
-    flag0 = 1;
+    int hartid = read_hartid();
+    flag0[hartid] = 1;
 }
 
 void ie_isr1(void) {
-    flag1 = 1;
+    int hartid = read_hartid();
+    flag1[hartid] = 1;
 }
 
 void ext_int_isr(void) {
-    ext_int_flag = 1;
+    int hartid = read_hartid();
+    ext_int_flag[hartid] = 1;
 }
 
 void ext_int_du_response(void) {
     printf("Got interrupt while in delay until (should not be early stopped by interrupt)\n");
-    du_int_triggered = 1;
+    int hartid = read_hartid();
+    du_int_triggered[hartid] = 1;
 }
 
 void ext_int_wu_response(void) {
     printf("Got interrupt while in wait until (should be stopped early by interrupt)\n");
-    wu_int_triggered = 1;
+    int hartid = read_hartid();
+    wu_int_triggered[hartid] = 1;
 }
 
 void ie_isr_get_time(void) {
-    isr_time = rdtime64();
+    int hartid = read_hartid();
+    isr_time[hartid] = rdtime64();
 }
 
-void test_two_interrupts(void) {
+void *test_two_interrupts(void *args) {
+    (void)(args);
+    int hartid = read_hartid();
+
     volatile uint32_t before, now, expire;
     register_isr(EXC_CAUSE_INTERRUPT_EXPIRE, ie_isr0);
     
@@ -49,10 +69,13 @@ void test_two_interrupts(void) {
     expire = before + EXPIRE_DELAY_NS;
     INTERRUPT_ON_EXPIRE(expire);
 
-    while (flag0 == 0);
+    ENABLE_INTERRUPTS();
+    while (flag0[hartid] == 0);
+    DISABLE_INTERRUPTS();
 
     now = rdtime();
-    fp_assert(now > expire, "Time is not as expected\n");
+    fp_assert(now > expire, "Time is not as expected (condition was %i > %i)\n",
+        now, expire);
 
     register_isr(EXC_CAUSE_INTERRUPT_EXPIRE, ie_isr1);
     
@@ -60,7 +83,9 @@ void test_two_interrupts(void) {
     expire = now + EXPIRE_DELAY_NS;
     INTERRUPT_ON_EXPIRE(expire);
 
-    while (flag1 == 0);
+    ENABLE_INTERRUPTS();
+    while (flag1[hartid] == 0);
+    DISABLE_INTERRUPTS();
 
     now = rdtime();
     fp_assert(now > expire, "Time is not as expected\n");
@@ -70,7 +95,10 @@ void test_two_interrupts(void) {
     // restored.
 }
 
-void test_disabled_interrupts(const uint32_t timeout_init) {
+void *test_disabled_interrupts(void *args) {
+    (void)(args);
+    int hartid = read_hartid();
+
     uint32_t timeout;
     volatile uint32_t now, expire;
 
@@ -80,10 +108,11 @@ void test_disabled_interrupts(const uint32_t timeout_init) {
     expire = now + EXPIRE_DELAY_NS;
     INTERRUPT_ON_EXPIRE(expire);
 
-    timeout = timeout_init;
-    while (flag0 == 0 && timeout--);
+    timeout = TIMEOUT_INIT;
+    // Do not enable interrupts
+    while (flag0[hartid] == 0 && timeout--);
 
-    fp_assert(flag0 == 0, "Interrupt occurred when disabled\n");
+    fp_assert(flag0[hartid] == 0, "Interrupt occurred when disabled\n");
 
     register_isr(EXC_CAUSE_INTERRUPT_EXPIRE, ie_isr1);
     
@@ -91,13 +120,17 @@ void test_disabled_interrupts(const uint32_t timeout_init) {
     expire = now + EXPIRE_DELAY_NS;
     INTERRUPT_ON_EXPIRE(expire);
 
-    timeout = timeout_init;
-    while (flag1 == 0 && timeout--);
+    timeout = TIMEOUT_INIT;
+    // Do not enable interrupts
+    while (flag1[hartid] == 0 && timeout--);
 
-    fp_assert(flag1 == 0, "Interrupt occurred when disabled\n");
+    fp_assert(flag1[hartid] == 0, "Interrupt occurred when disabled\n");
 }
 
-void test_low_timeout(void) {
+void *test_low_timeout(void *args) {
+    (void) (args);
+    int hartid = read_hartid();
+
     volatile uint32_t now, expire;
 
     // Feel free to play around with this value and see what happens
@@ -123,16 +156,21 @@ void test_low_timeout(void) {
      * 
      */
     INTERRUPT_ON_EXPIRE(expire);
-    while (flag0 == 0);
+    ENABLE_INTERRUPTS();
+    while (flag0[hartid] == 0);
+    DISABLE_INTERRUPTS();
 }
 
-void test_fp_delay_until(void) {
+void *test_fp_delay_until(void *args) {
+    (void)(args);
+    int hartid = read_hartid();
+
     // Delay until should execute all interrupts but keep sleeping until the
     // specified timeout has occurred
     volatile uint32_t now, expire, delay;
     const uint32_t timeout_ns = EXPIRE_DELAY_NS;
 
-    isr_time = 0;
+    isr_time[hartid] = 0;
     register_isr(EXC_CAUSE_INTERRUPT_EXPIRE, ie_isr_get_time);
 
     now = rdtime();
@@ -140,23 +178,28 @@ void test_fp_delay_until(void) {
     delay = expire + timeout_ns;
 
     INTERRUPT_ON_EXPIRE(expire);
+    ENABLE_INTERRUPTS();
     fp_delay_until(delay);
+    DISABLE_INTERRUPTS();
 
     now = rdtime();
 
-    fp_assert(isr_time != 0, "Interrupt did not occur\n");
+    fp_assert(isr_time[hartid] != 0, "Interrupt did not occur\n");
     fp_assert(now > delay, "Delay until did not delay full duration\n");
-    fp_assert(expire < isr_time && isr_time < delay, "Interrupt did not occur during delay until\n");
+    fp_assert(expire < isr_time[hartid] && isr_time[hartid] < delay, "Interrupt did not occur during delay until\n");
 }
 
-void test_fp_wait_until(void) {
+void *test_fp_wait_until(void *args) {
+    (void) (args);
+    int hartid = read_hartid();
+
     // Wait until should sleep until an interrupt occurs or the timeout value is
     // reached. If an interrupt occurs, it should execute it and continue
     // execution (i.e., stop sleeping).
     volatile uint32_t before, now, expire, delay;
     const uint32_t timeout_ns = EXPIRE_DELAY_NS;
 
-    isr_time = 0;
+    isr_time[hartid] = 0;
     register_isr(EXC_CAUSE_INTERRUPT_EXPIRE, ie_isr_get_time);
     
     before = rdtime();
@@ -164,78 +207,36 @@ void test_fp_wait_until(void) {
     delay = expire + timeout_ns;
 
     INTERRUPT_ON_EXPIRE(expire);
+    ENABLE_INTERRUPTS();
     fp_wait_until(delay);
+    DISABLE_INTERRUPTS();
 
     now = rdtime();
 
-    fp_assert(isr_time != 0, "Interrupt did not occur\n");
+    fp_assert(isr_time[hartid] != 0, "Interrupt did not occur\n");
     fp_assert(expire < now && now < delay, "Time not as expected\n",
         expire, now, delay);
-    fp_assert(before < isr_time && 
-              expire < isr_time && 
-              isr_time < now && 
-              isr_time < delay, 
+    fp_assert(before < isr_time[hartid] && 
+              expire < isr_time[hartid] && 
+              isr_time[hartid] < now && 
+              isr_time[hartid] < delay, 
               "Interrupt did not occur when expected\n"
     );
 }
 
-int main(void) {
-    // Will enable and disable interrupts before and after each test to avoid
-    // getting weird errors with printing
-
-    // Test that interrupts work
-    ENABLE_INTERRUPTS();
-    test_two_interrupts();
-    DISABLE_INTERRUPTS();
-    printf("1st run: interrupts ran sucessfully with two different ISRs\n");
-
-    flag0 = 0;
-    flag1 = 0;
-
-    // Test that they work again; i.e., there are no side effects of the first
-    // test
-    ENABLE_INTERRUPTS();
-    test_two_interrupts();
-    DISABLE_INTERRUPTS();
-    printf("2nd run: interrupts ran sucessfully with two different ISRs\n");
-
-    flag0 = 0;
-    flag1 = 0;
-
-    // Try to disable interrupts and check that no interrupts were called
-    DISABLE_INTERRUPTS();
-    test_disabled_interrupts(10000);
-    ENABLE_INTERRUPTS();
-    printf("3rd run: interrupts were disabled and none were triggered\n");
-
-    // No need to reset flags if the interrupts were not run
-
-    ENABLE_INTERRUPTS();
-    test_low_timeout();
-    DISABLE_INTERRUPTS();
-    printf("4th run: interrupts ran sucessfully with low timeout\n");
-
-    flag0 = 0;
-    flag1 = 0;
-
-    ENABLE_INTERRUPTS();
-    test_fp_delay_until();
-    DISABLE_INTERRUPTS();
-    printf("5th run: delay until ran sucessfully\n");
-
-    flag0 = 0;
-    flag1 = 0;
-
-    ENABLE_INTERRUPTS();
-    test_fp_wait_until();
-    DISABLE_INTERRUPTS();
-    printf("6th run: wait until ran sucessfully\n");
+void *test_external_interrupt(void *args) {
+    (void) (args);
+    int hartid = read_hartid();
 
     register_isr(EXC_CAUSE_EXTERNAL_INT, ext_int_isr);
     ENABLE_INTERRUPTS();
-    while (ext_int_flag == 0);
+    while (ext_int_flag[hartid] == 0);
     DISABLE_INTERRUPTS();
-    printf("7th run: got external interrupt\n");
+}
+
+void *test_du_not_stopped_by_int(void *args) {
+    (void) (args);
+    int hartid = read_hartid();
 
     register_isr(EXC_CAUSE_EXTERNAL_INT, ext_int_du_response);
     volatile uint64_t before, delay, after;
@@ -248,14 +249,22 @@ int main(void) {
     DISABLE_INTERRUPTS();
     after = rdtime64();
 
-    if (du_int_triggered) {
+    if (du_int_triggered[hartid]) {
         fp_assert(before + delay < after, "User was able to stop delay until instruction before timer had run out");
-        printf("8th run: delay for not stopped early\n");
+        return NULL;
     } else {
         printf("Warning: User should provide interrupt to test this feature\n");
+        return (void *) 1;
     }
+}
+
+void *test_wu_stopped_by_int(void *args) {
+    (void) (args);
+    int hartid = read_hartid();
 
     register_isr(EXC_CAUSE_EXTERNAL_INT, ext_int_wu_response);
+    volatile uint64_t before, delay, after;
+
     before = rdtime64();
     delay = (int) 10000000;
     
@@ -264,13 +273,11 @@ int main(void) {
     DISABLE_INTERRUPTS();
     after = rdtime64();
 
-    if (wu_int_triggered) {
+    if (wu_int_triggered[hartid]) {
         fp_assert(before + delay > after, "User was unable to stop wait until instruction before timer had run out\n");
-        printf("9th run: wait for stopped early\n");
+        return NULL;
     } else {
         printf("Warning: User should provide interrupt to test this feature\n");
+        return (void *) 1;
     }
-
-
-    return 0;
 }
