05012ab65c103af22bfbe0b601642d2aaf96ad2d
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-04-30 17:08:13 +0200

    Remove redundant files

--- fpga/zedboard/fp-blinky/fpga.mk
+++
@@ -1,6 +0,0 @@
-FPGA_PROGRAM_NAME := blinky
-FPGA_PROGRAM_PATH := $(FLEXPRET_ROOT_DIR)/programs/tests/c-tests/$(FPGA_PROGRAM_NAME)/$(FPGA_PROGRAM_NAME).mem
-
-$(FPGA_PROGRAM_NAME): 
-	make -C programs/tests/c-tests/$(FPGA_PROGRAM_NAME) clean all TARGET=FPGA
-	cp $(FPGA_PROGRAM_PATH) fpga/$(FPGA_BOARD)/fp-blinky/ispm.mem
