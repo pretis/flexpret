a802040bd02ff7660fd9dd21bc3f551b66e7b37c
erlingrj <erling.jellum@gmail.com>
2024-05-05 11:52:18 +0200

    More env.fish fixes

--- env.fish
+++ env.fish
@@ -1,7 +1,5 @@
 # From: https://stackoverflow.com/questions/19672673/can-a-fish-script-tell-what-directory-its-stored-in
-set FLEXPRET_ROOT (cd (dirname (status -f)); and pwd)
-
-set DIR (cd (dirname (status -f)); and pwd) 
-set PATH $PATH:$FLEXPRET_ROOT/build/emulator
-set FP_PATH $FLEXPRET_ROOT
-set FP_SDK_PATH $FLEXPRET_ROOT/sdk
+set -g FLEXPRET_ROOT (cd (dirname (status -f)); and pwd)
+set -g PATH $PATH:$FLEXPRET_ROOT/build/emulator
+set -g FP_PATH $FLEXPRET_ROOT
+set -g FP_SDK_PATH $FLEXPRET_ROOT/sdk
