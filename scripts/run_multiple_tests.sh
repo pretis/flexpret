4aa0ac6f47a848990a3ed7df4ef3a2702162fc93
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-05-14 16:07:00 +0200

    Add debug on CI test script

--- scripts/run_multiple_tests.sh
+++ scripts/run_multiple_tests.sh
@@ -9,7 +9,7 @@ run_test() {
     cd $FP_PATH && cmake -DFP_CONFIG=$1 -B build && cmake --build build && cmake --install build
     
     # Build SDK and run tests
-    cd $FP_SDK_PATH && cmake -B build && cmake --build build && ctest --test-dir build
+    cd $FP_SDK_PATH && cmake -B build && cmake --build build && ctest --test-dir build --output-on-failure
 }
 
 # These configurations correspond with the files inside `./flexpret/cmake/configs`
