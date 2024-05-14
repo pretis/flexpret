ba2a7c627af5ea0caca977a8ceabfeced4a718ee
Magnus Mæhlum <magnusmaehlum@outlook.com>
2024-05-14 16:19:04 +0200

    Attempt fix CI script

--- scripts/run_multiple_tests.sh
+++ scripts/run_multiple_tests.sh
@@ -3,7 +3,7 @@ set -e
 # $1: Which config to run
 run_test() {
     # Get a clean state
-    rm -rf $FP_PATH/build && rm -rf $FP_SDK_PATH/build
+    rm -rf $FP_PATH/build && rm -rf $FP_SDK_PATH/build && rm -rf $FP_SDK_PATH/flexpret
     
     # Build FlexPRET
     cd $FP_PATH && cmake -DFP_CONFIG=$1 -B build && cmake --build build && cmake --install build
@@ -18,6 +18,7 @@ configs=(
 )
 
 # Run test on each config
-for cfg in "${configs[@]}"; do \
+for cfg in "${configs[@]}"
+do
     run_test $cfg
 done
